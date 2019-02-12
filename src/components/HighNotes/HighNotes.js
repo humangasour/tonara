744d2e4fbda24b598eb31d071b8d1d5d0251f3c5
Anuj Sachdeva <anujsachdeva100@gmail.com>
2019-02-12 11:56:02 +0530

    white keys between the black ones clickable now

--- src/components/HighNotes/HighNotes.js
+++ src/components/HighNotes/HighNotes.js
@@ -5,13 +5,19 @@ import './HighNotes.css';
 import Key from '../Key/Key';
 
 const HighNotes = (props) =>  {
+	const notes = Object.keys(props.notes); //converting the notes recieved into array to iterate over
+
 	// generating keys by iterating the data recieved as props
-	const keys = Object.keys(props.notes).map((note, index) => {
+	const keys = notes.map((note, index) => {
 		return <Key key={index} note={props.notes[note].url} />
 	});
 
+	const style = {
+		"grid-template-columns": `repeat(${notes.length}, 85px)`,
+	}
+
 	return ( 
-		<div className="Keyboard--white">
+		<div className="Keyboard--white" style={{...style}}>
      {keys}
     </div>
 	)
